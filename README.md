5fd2d61962363cf898fa67d35d61b47598e74ecd
zhangfazhan <37731167+zhangfazhan@users.noreply.github.com>
2019-07-11 09:58:10 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -9,15 +9,21 @@
 - 参考[How to Write a Spelling Corrector](https://norvig.com/spell-correct.html)
 - 中文纠错将中文转换成拼音，再利用英文原理进行纠错
 
-## 示例（词库采用《水浒传》）
+## 示例
 ```python
 
-print(correct('松江'))
-print(correct('李奎'))
-print(correct('吴宋'))
+    spell_correct = SpellCorrect(train=False)
+    print(spell_correct.correct('宋江'))
+    print(spell_correct.correct('松江'))
+    print(spell_correct.correct('李奎'))
+    print(spell_correct.correct('吴宋'))
+    print(spell_correct.correct('送三连'))
 
-宋江
-李逵
-武松
+output:
+['宋江', '松江', '宋将']
+['宋江', '松江', '宋将']
+['李逵']
+['武松']
+['宋三郎', '宋万先']
 ```
 
--- spell_correct_cn.py
+++ spell_correct_cn.py
@@ -1,90 +1,106 @@
 # -*- coding: utf-8 -*-
 
+import os
 import re
 import jieba
 import json
 from xpinyin import Pinyin
 from collections import Counter
 
-p = Pinyin()
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+corpus_path = os.path.join(CURRENT_DIR, 'data', '水浒传.txt')
+stopwords_path = os.path.join(CURRENT_DIR, 'data', 'stopwords.txt')
+pinyin_word_path = os.path.join(CURRENT_DIR, 'data', 'pinyin_word.json')
+word_count_path = os.path.join(CURRENT_DIR, 'data', 'word_count.json')
 
 
-### 纠错词库准备，将文本转换成拼音，保存成json格式,（这里选用的是《水浒传》）###
+class SpellCorrect():
+    def __init__(self, corpus_path=corpus_path, train=True, ret_num=10):
+        """
+        :param corpus_path: 文本路径
+        :param train: 是否根据文本，生成纠错词库，默认文本为《水浒传》
+        :param ret_num: 返回可能的纠错结果数目
+        """
+        self.p = Pinyin()
+        self.corpus_path = corpus_path
+        self.ret_num = ret_num
+        if train:
+            self.split_text()
 
-def load_stopwords(stopwords_path='./data/stopwords.txt'):
-    with open(stopwords_path, encoding='utf-8') as f:
-        return [line.strip() for line in f]
+        self.pinyin_word = self.load_json(pinyin_word_path)
+        self.word_count = self.load_json(word_count_path)
+        self.WORDS = self.pinyin_word.keys()
 
-def split_text(file_path='./data/水浒传.txt'):
-    pinyin_word = {}  # 存放拼音对应词
-    stopwords = load_stopwords()
-    with open(file_path, encoding='utf-8') as f:
-        words = jieba.lcut(re.sub("[\sa-z0-9\r\n]+", "", f.read()))
-        word_count = dict(Counter(words))  # 词对应词频
-        for key in word_count:
-            if key not in stopwords:
-                pinyin = p.get_pinyin(key).replace('-', '')
-                if pinyin not in pinyin_word:
-                    pinyin_word[pinyin] = [key]
-                else:
-                    pinyin_word[pinyin].append(key)
+    def load_stopwords(self):
+        with open(stopwords_path, encoding='utf-8') as f:
+            return [line.strip() for line in f]
 
-    with open('./data/word_count.json', 'w', encoding='utf-8') as f:
-        json.dump(word_count, f, ensure_ascii=False, indent=4)
+    def split_text(self):
+        pinyin_word = {}  # 存放拼音对应词
+        stopwords = self.load_stopwords()
+        with open(self.corpus_path, encoding='utf-8') as f:
+            words = jieba.lcut(re.sub("[\sa-z0-9\r\n]+", "", f.read()))
+            word_count = dict(Counter(words))  # 词对应词频
+            for key in word_count:
+                if key not in stopwords:
+                    pinyin = self.p.get_pinyin(key, splitter='')
+                    if pinyin not in pinyin_word:
+                        pinyin_word[pinyin] = [key]
+                    else:
+                        pinyin_word[pinyin].append(key)
+        self.dump_json(word_count, word_count_path)
+        self.dump_json(pinyin_word, pinyin_word_path)
 
-    with open('./data/pinyin_word.json', 'w', encoding='utf-8') as f:
-        json.dump(pinyin_word, f, ensure_ascii=False, indent=4)
+    def candidates(self, word):
+        "Generate possible spelling corrections for word."
+        return (self.known([word]) or self.known(self.edits1(word)) or self.known(self.edits2(word)) or [word])
 
+    def known(self, words):
+        "The subset of `words` that appear in the dictionary of WORDS."
+        return set(w for w in words if w in self.WORDS)
 
-##### copy from spell_correct_en.py ######
-def candidates(word):
-    "Generate possible spelling corrections for word."
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+    def edits1(self, word):
+        "All edits that are one edit away from `word`."
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes = [L + R[1:] for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+        replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+        inserts = [L + c + R for L, R in splits for c in letters]
+        return set(deletes + transposes + replaces + inserts)
 
-def known(words):
-    "The subset of `words` that appear in the dictionary of WORDS."
-    return set(w for w in words if w in WORDS)
+    def edits2(self, word):
+        "All edits that are two edits away from `word`."
+        return (e2 for e1 in self.edits1(word) for e2 in self.edits1(e1))
 
-def edits1(word):
-    "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
-    return set(deletes + transposes + replaces + inserts)
+    def load_json(self, json_file_path):
+        with open(json_file_path, encoding='utf-8') as f:
+            return json.loads(f.read(), encoding='utf-8')
 
-def edits2(word):
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+    def dump_json(self, content, json_file_path):
+        with open(json_file_path, 'w', encoding='utf-8') as f:
+            json.dump(content, f, ensure_ascii=False, indent=4)
 
-#### 利用拼音进行纠错  #####
-
-def load_json(json_file_path):
-    with open(json_file_path, encoding='utf-8') as f:
-        return json.loads(f.read(), encoding='utf-8')
-
-pinyin_word = load_json('./data/pinyin_word.json')
-word_count = load_json('./data/word_count.json')
-WORDS = pinyin_word.keys()
-
-
-def correct(word):
-    word_pinyin = p.get_pinyin(word).replace('-', '')
-    candidate_pinyin = candidates(word_pinyin)
-    ret_dic = {}
-    words = []
-    for pinyin in candidate_pinyin:
-        words.extend(pinyin_word[pinyin])
-    for word in words:
-        ret_dic[word] = word_count.get(word, 0)
-    sort_word = sorted(ret_dic.items(), key=lambda x: x[1], reverse=True)
-    return sort_word[0][0]
+    def correct(self, word):
+        word_pinyin = self.p.get_pinyin(word, splitter='')
+        candidate_pinyin = self.candidates(word_pinyin)
+        ret_dic = {}
+        words = []
+        for pinyin in candidate_pinyin:
+            words.extend(self.pinyin_word[pinyin])
+        for word in words:
+            ret_dic[word] = self.word_count.get(word, 0)
+        sort_word = sorted(ret_dic.items(), key=lambda x: x[1], reverse=True)
+        return [item[0] for item in sort_word[:self.ret_num]]
 
 
 if __name__ == '__main__':
-    print(correct('松江'))
-    print(correct('李奎'))
-    print(correct('吴宋'))
+    spell_correct = SpellCorrect()
+    print(spell_correct.correct('宋江'))
+    print(spell_correct.correct('松江'))
+    print(spell_correct.correct('李奎'))
+    print(spell_correct.correct('吴宋'))
+    print(spell_correct.correct('送三连'))
+
+
 
